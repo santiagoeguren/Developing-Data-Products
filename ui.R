92475ac563aa4285ede7b070c40e34be4c8bc54b
santiagoeguren <santiago.e@live.com.ar>
2016-07-15 13:18:19 -0300

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,44 +1,53 @@
 
-
 library(shiny)
+library(plotly)
 
 shinyUI(fluidPage(
-  titlePanel(title=h4("Modeling Stock Price with Quadratic Discriminant Analysis")),
+  titlePanel(title=h4("Modeling Stock Price with Quadratic Discriminant Analysis",style = "color:blue" ) ),
 
   sidebarLayout(
     sidebarPanel(
+      #h6(imageOutput("img")),
+
                  textInput("Stock","Stock Symbol","BA"),
                  textInput("start","Date Start","2012-01-01"),
                  textInput("finish","Date End","2016-06-01"),
 
-                 sliderInput("slider","% of Data for Training",min=10,max = 100,value =10,step = 10 )
+                  sliderInput("slider","% of Data for Training",min=10,max = 100,value =10,step = 10 )
 
                  ),
 
    mainPanel(
 
-     p("The application is built for Developing Data Products class
-from Coursera. This application allows determinate if Quadratic Discriminant Analysis can predict
-       the actual direction of price using lag1 and lag2."),
-
-     h5("References"),
-
-     a(href="http://finance.yahoo.com/lookup","Find a symbol"),p("On Yahoo"),
-
-     a(href="https://github.com/RanjanParida/DevelopingDataProd",
-       "SourceCode"), p("On GITHUB"),
-
-     a(href="http://rpubs.com/rparida/DDP_PA",
-       "Course Presentation"), p("On RPUBs"),
 
      tabsetPanel(type="tab",
 
-                 tabPanel("QDA",p(""),textOutput("text1")),
-                 tabPanel("Plot",plotOutput("plot1")),
-                 tabPanel("Data",tableOutput("data")),
-                 tabPanel("Help",p(""),p("-Example of Symbols:KO - Coca-cola,AAPL - Apple Inc, JNJ -Johnson & Johnson...etc"),p("-lag1 y lag2: Change of price time -1 and -2"))
+                 tabPanel("QDA",p(""),HTML('<center><img src="i1.jpg" width="300"></center>'), p("The application is built for Developing Data Products class
+from Coursera. This application allows determinate if Quadratic Discriminant Analysis can predict
+                                                                                                 the actual direction (test price) using past price (training).",style = "color:blue")
 
-               )
+                          ),
+                 tabPanel("Model",textOutput("text1"),p(""),p(""),p(""),plotlyOutput("plot2")),
+                 tabPanel("Plot lag1 vs lag2",plotOutput("plot1")),
+                 tabPanel("Data",tableOutput("data")),
+                 tabPanel("Help",p(""),HTML('<center><img src="help.png" width="700"></center>'))
+
+     ),
+
+
+     #p(textOutput("text1"),style = "color:blue"),
+     #plotlyOutput("plot2"),
+
+     a(href="http://finance.yahoo.com/lookup","Find a symbol"),p("On Yahoo"),
+
+     a(href="https://github.com/santiagoeguren/Developing-Data-Products",
+       "SourceCode"), p("On GITHUB"),
+
+     a(href="http://rpubs.com/santiagoe/DevelopingDataProducts",
+       "Course Presentation"), p("On RPUBs")
+
+
+
 
      )## fin main panel
 
@@ -46,3 +55,7 @@ from Coursera. This application allows determinate if Quadratic Discriminant Ana
 
 )
 ))
+
+
+
+
